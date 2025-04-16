3451bdfb43cceb49f62dd5df84c0d47411b51ecb
Lesya <117307391+Lesyalys@users.noreply.github.com>
2025-04-16 18:43:43 +0500

    Updates

--- assets/index-BIAG-XDs.js
+++ assets/index-BIAG-XDs.js
@@ -1,4 +1,4 @@
-(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))t(e);new MutationObserver(e=>{for(const i of e)if(i.type==="childList")for(const n of i.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&t(n)}).observe(document,{childList:!0,subtree:!0});function s(e){const i={};return e.integrity&&(i.integrity=e.integrity),e.referrerPolicy&&(i.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?i.credentials="include":e.crossOrigin==="anonymous"?i.credentials="omit":i.credentials="same-origin",i}function t(e){if(e.ep)return;e.ep=!0;const i=s(e);fetch(e.href,i)}})();function o(l){l.innerHTML=`
+(function(){const l=document.createElement("link").relList;if(l&&l.supports&&l.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const i of e)if(i.type==="childList")for(const t of i.addedNodes)t.tagName==="LINK"&&t.rel==="modulepreload"&&n(t)}).observe(document,{childList:!0,subtree:!0});function a(e){const i={};return e.integrity&&(i.integrity=e.integrity),e.referrerPolicy&&(i.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?i.credentials="include":e.crossOrigin==="anonymous"?i.credentials="omit":i.credentials="same-origin",i}function n(e){if(e.ep)return;e.ep=!0;const i=a(e);fetch(e.href,i)}})();function s(r){r.innerHTML=`
     <div class = "gameCompUl">    
     
     <ul>
@@ -31,7 +31,7 @@
         <li class="underHearUl"><p id="underHeader">See more:</p></li>
         <li class="underHearUl" id="iconArtstation"></li>
       </ul>
-      `}function d(l){l.innerHTML=`
+      `}function c(r){r.innerHTML=`
   <div>
     <img src="https://i.pinimg.com/736x/8a/39/5c/8a395c1f432ecb52c04c8a023ef4647b.jpg" class ="gameComp" alt="JavaScript logo" />
 
@@ -42,14 +42,14 @@
 
       <ul>
         <li class="underHearUl"><p id="underHeader">mail:</p></li>
-        <li class="underHearUl"><img src ="/public/nodejs.png"></li>
+        <li class="underHearUl"><a href="https://e.mail.ru/cgi-bin/sentmsg?To=nya.olesya@bk.ru&from=otvet&afterReload=1" target="_blank">nya.olesya@bk.ru</li>
       </ul>
 
       <ul>
         <li class="underHearUl"><p id="underHeader">tg:</p></li>
-        <li class="underHearUl"><img src ="/public/nodejs.png"></li>
+        <li class="underHearUl"><a href="https://e.mail.ru/cgi-bin/sentmsg?To=nya.olesya@bk.ru&from=otvet&afterReload=1" target="_blank">nya.olesya@bk.ru</li>
       </ul>
-      `}function c(l){l.innerHTML=`
+      `}function o(r){r.innerHTML=`
     <img id="iconMe" src ="https://i.pinimg.com/736x/4c/a1/07/4ca1075720f674d2e101118c3e38d37f.jpg">
       <h1>I'm Lesya, a beginner designer and frontend developer
         <p id="underHeader">Glad to see you!</p>
@@ -64,4 +64,4 @@
     <canvas id="demonRive"></canvas>
 
     </div>
-    `,l.querySelector("#workBtn").addEventListener("click",()=>o(document.querySelector("#MyAPPP"))),l.querySelector("#contactBtn").addEventListener("click",()=>d(document.querySelector("#MyAPPP"))),l.querySelector("#iconMe").addEventListener("click",()=>c(document.querySelector("#header")))}c(document.querySelector("#header"));
+    `,r.querySelector("#workBtn").addEventListener("click",()=>s(document.querySelector("#MyAPPP"))),r.querySelector("#contactBtn").addEventListener("click",()=>c(document.querySelector("#MyAPPP"))),r.querySelector("#iconMe").addEventListener("click",()=>o(document.querySelector("#header")))}o(document.querySelector("#header"));
