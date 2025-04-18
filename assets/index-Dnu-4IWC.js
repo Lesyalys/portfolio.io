64a3813e254de3246c8cee80b0c25602aebcdcc9
Lesya <117307391+Lesyalys@users.noreply.github.com>
2025-04-18 22:02:14 +0500

    Updates

--- assets/index-Dnu-4IWC.js
+++ assets/index-Dnu-4IWC.js
@@ -1,4 +1,4 @@
-(function(){const l=document.createElement("link").relList;if(l&&l.supports&&l.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const i of e)if(i.type==="childList")for(const t of i.addedNodes)t.tagName==="LINK"&&t.rel==="modulepreload"&&n(t)}).observe(document,{childList:!0,subtree:!0});function a(e){const i={};return e.integrity&&(i.integrity=e.integrity),e.referrerPolicy&&(i.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?i.credentials="include":e.crossOrigin==="anonymous"?i.credentials="omit":i.credentials="same-origin",i}function n(e){if(e.ep)return;e.ep=!0;const i=a(e);fetch(e.href,i)}})();function s(r){r.innerHTML=`
+(function(){const l=document.createElement("link").relList;if(l&&l.supports&&l.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const i of e)if(i.type==="childList")for(const t of i.addedNodes)t.tagName==="LINK"&&t.rel==="modulepreload"&&n(t)}).observe(document,{childList:!0,subtree:!0});function a(e){const i={};return e.integrity&&(i.integrity=e.integrity),e.referrerPolicy&&(i.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?i.credentials="include":e.crossOrigin==="anonymous"?i.credentials="omit":i.credentials="same-origin",i}function n(e){if(e.ep)return;e.ep=!0;const i=a(e);fetch(e.href,i)}})();function c(r){r.innerHTML=`
     <div class = "gameCompUl">    
     
     <ul>
@@ -31,7 +31,7 @@
         <li class="underHearUl"><p id="underHeader">See more:</p></li>
         <li class="underHearUl" id="iconArtstation"></li>
       </ul>
-      `}function c(r){r.innerHTML=`
+      `}function s(r){r.innerHTML=`
   <div>
     <img src="https://i.pinimg.com/736x/8a/39/5c/8a395c1f432ecb52c04c8a023ef4647b.jpg" class ="gameComp" alt="JavaScript logo" />
 
@@ -42,15 +42,15 @@
 
       <ul>
         <li class="underHearUl"><p id="underHeader">mail:</p></li>
-        <li class="underHearUl"><a href="https://e.mail.ru/cgi-bin/sentmsg?To=nya.olesya@bk.ru&from=otvet&afterReload=1" target="_blank">nya.olesya@bk.ru</li>
+        <li class="underHearUl"><a href="https://e.mail.ru/cgi-bin/sentmsg?To=nya.olesya@bk.ru&from=otvet&afterReload=1" target="_blank">nya.olesya@bk.ru</a></li>
       </ul>
 
       <ul>
         <li class="underHearUl"><p id="underHeader">tg:</p></li>
-        <li class="underHearUl"><a href="https://e.mail.ru/cgi-bin/sentmsg?To=nya.olesya@bk.ru&from=otvet&afterReload=1" target="_blank">nya.olesya@bk.ru</li>
+        <li class="underHearUl"><a href="https://t.me/Shadowtav" target="_blank">@Shadowtav</a></li>
       </ul>
       `}function o(r){r.innerHTML=`
-    <img id="iconMe" src ="https://i.pinimg.com/736x/4c/a1/07/4ca1075720f674d2e101118c3e38d37f.jpg">
+      <img id="iconMe" src ="https://i.pinimg.com/736x/4c/a1/07/4ca1075720f674d2e101118c3e38d37f.jpg">
       <h1>I'm Lesya, a beginner designer and frontend developer
         <p id="underHeader">Glad to see you!</p>
       </h1>
@@ -60,8 +60,4 @@
         <!-- <li><button><p>WORK</p></button></li> -->
       </ul>
     `,document.querySelector("#MyAPPP").innerHTML=`
-    <div>
-    <canvas id="demonRive"></canvas>
-
-    </div>
-    `,r.querySelector("#workBtn").addEventListener("click",()=>s(document.querySelector("#MyAPPP"))),r.querySelector("#contactBtn").addEventListener("click",()=>c(document.querySelector("#MyAPPP"))),r.querySelector("#iconMe").addEventListener("click",()=>o(document.querySelector("#header")))}o(document.querySelector("#header"));
+    `,r.querySelector("#workBtn").addEventListener("click",()=>c(document.querySelector("#MyAPPP"))),r.querySelector("#contactBtn").addEventListener("click",()=>s(document.querySelector("#MyAPPP"))),r.querySelector("#iconMe").addEventListener("click",()=>o(document.querySelector("#header")))}o(document.querySelector("#header"));
